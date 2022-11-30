7603ac41ed072f9a0fd430dbb297c828bf1a7f0a
Chris Vo <vo.chris.k@gmail.com>
2022-11-29 18:23:22 -0800

    add hero images and update navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,34 +6,45 @@ import { Color } from '../models';
 export const Navigation = styled.nav`
   background-color: ${ Color.BackgroundColor };
   box-shadow: 1px 3px 10px #000;
-  // justify-content: space-between;
   position: fixed;
   top: 0;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
   width: 100%;
   height: 4.0625rem;
 `;
 
 export const LinkList = styled.ul`
   display: flex;
-  justify-content: flex-end;
+  counter-reset: listCounter;
 `;
 
 export const List = styled.li`
   color: ${ Color.TextColor };
   cursor: pointer;
-  font-size: 1.125rem;
+  font-size: 1.25rem;
   list-style: none;
-  margin: 0 2.5rem;
+  margin: 0 1.5rem;
+
+  :before {
+    content: "0" counter(listCounter) ". ";
+    counter-increment: listCounter;
+    color: ${ Color.HeaderColor };
+    font-size: 1.25rem;
+    margin-right: 0.625rem;
+  }
 `;
 
 export const Navbar = () => {
   return (
     <Navigation>
       <LinkList>
-        <List>test</List>
-        <List>123</List>
-        <List>3213</List>
-        <List>111</List>
+        <List>About</List>
+        <List>Experience</List>
+        <List>Projects</List>
+        <List>Contact</List>
+        <List>Resume</List>
       </LinkList>
     </Navigation>
   );
